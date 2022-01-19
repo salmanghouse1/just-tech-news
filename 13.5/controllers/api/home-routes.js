5d4963b4f187738882e039c8db720bbb1b094ec7
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-01-19 15:07:36 -0500

    homepage

---
+++ 13.5/controllers/api/home-routes.js
@@ -0,0 +1,9 @@
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+
+const router = require('express').Router();
+
+
+
+module.exports = router;
---
+++ 13.5/controllers/index.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+router.get('/', (req, res) => {
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        vote_count: 10,
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
+});
+
+module.exports = router;
